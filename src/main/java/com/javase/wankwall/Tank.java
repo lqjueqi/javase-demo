180aded4e0c80da1e61f16cef5d2a3fe19d4e7c6
lqjueqi <39261089+lqjueqi@users.noreply.github.com>
2022-02-24 17:17:10 +0800

    boundary check

--- src/main/java/com/javase/wankwall/Tank.java
+++ src/main/java/com/javase/wankwall/Tank.java
@@ -107,7 +107,21 @@ public class Tank {
                 y += SPEED;
                 break;
         }
-        if (random.nextInt(10) > 8) this.fire();
+        if (this.group==Group.BAD && random.nextInt(100) > 95) this.fire();
+        if(this.group == Group.BAD && random.nextInt(100) > 95)
+            randomDir();
+        boundsCheck();
+    }
+
+    private void boundsCheck() {
+        if(this.x < 2) x = 2;
+        if (this.y < 28) y = 28;
+        if (this.x > TankFrame.GAME_WIDTH- Tank.WIDTH -2) x = TankFrame.GAME_WIDTH - Tank.WIDTH -2;
+        if (this.y > TankFrame.GAME_HEIGHT - Tank.HEIGHT -2 ) y = TankFrame.GAME_HEIGHT -Tank.HEIGHT -2;
+    }
+
+    private void randomDir() {
+        this.dir = Dir.values()[random.nextInt(4)];
     }
 
     public void fire() {
--- src/main/java/com/javase/wankwall/TankFrame.java
+++ src/main/java/com/javase/wankwall/TankFrame.java
@@ -59,7 +59,7 @@ public class TankFrame extends Frame {
         g.setColor(Color.WHITE);
         g.drawString("子弹的数量：" + bullets.size(), 10, 60);
         g.drawString("敌人的数量：" + tanks.size(), 10, 80);
-        g.drawString("敌人的数量：" + tanks.size(), 10, 80);
+        g.drawString("爆炸的数量:" + explodes.size(), 10, 100);
         g.setColor(c);
         myTank.paint(g);
         for (int i = 0; i < bullets.size(); i++) {
